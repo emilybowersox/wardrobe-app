62dd5985f3e3b63c0d6373e2eb087c5998649131
Emily Bowersox <emily.bowersox100@gmail.com>
2021-01-19 15:47:37 -0600

    creating repository

---
+++ src/main/java/com/codeup/wardrobeapp/WardrobeAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.wardrobeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WardrobeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/wardrobeapp/WardrobeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.wardrobeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
